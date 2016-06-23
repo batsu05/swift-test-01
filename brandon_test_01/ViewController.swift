cef803ca493cc027fffecfd5a4a40516917a76df
user there <m>
2016-06-23 22:24:18 +0900

    Initial Commit

---
+++ brandon_test_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  brandon_test_01
+//
+//  Created by brandon on 2016. 6. 23..
+//  Copyright © 2016년 brandon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
